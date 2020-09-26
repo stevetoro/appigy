427c9c1fb31d44c7e49a3b491361f2ac4f49532b
Steve Toro <steve.toro@gmail.com>
2020-09-26 06:23:39 -0400

    ContentView should render AppList, not Text.

--- appigy/ContentView.swift
+++ appigy/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        AppList()
             .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
